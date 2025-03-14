af6e977cb4171412c86c96e92dc4ac715be5f22f
Maxim Meleshkin <maximmms@hotmail.com>
2025-03-14 21:47:13 +0300

    Optimization and documentation

--- main.py
+++ main.py
@@ -24,26 +24,47 @@ buttons = []
 
 
 def show_hint(*lines):
+    """
+    Функция для отображения подсказки пользовтелю
+    :param lines: Произвольное количество строк, которые будут объединены в одну подсказку.
+    :return: Объединненая строка подсказки
+    """
     return '\n'.join(lines)
 
 
 def show_target(data):
+    """
+    Функция для отображения целевого слова и его перевода.
+    :param data: Словарь содержащий целевое слово и его перевод
+    :return: Строка с целевым словом и переводом
+    """
     return f"{data['target_word']} -> {data['translate_word']}"
 
 
 class Command:
+    """
+    Класс, содержащий команды для взаимодействия с пользователем
+    """
     ADD_WORD = 'Добавить слово ➕'
     DELETE_WORD = 'Удалить слово🔙'
     NEXT = 'Дальше ⏭'
 
 
 class MyStates(StatesGroup):
+    """
+    Класс для упарвления состоянием бота
+    """
     target_word = State()
     translate_word = State()
     another_words = State()
 
 
 def get_user_step(uid):
+    """
+    Функция для получения текущего шага пользователя.
+    :param uid: Уникальный идентификатор пользователя.
+    :return: Текущий шаг пользователя
+    """
     if uid not in known_users:
         known_users.add(uid)
         userStep[uid] = 0
@@ -52,20 +73,33 @@ def get_user_step(uid):
 
 
 def create_markup(buttons):
+    """
+    Функция для создания клавиатуры с кнопками.
+    :param buttons: Список кнопок клавиатуры
+    :return:Клавиатура
+    """
     markup = types.ReplyKeyboardMarkup(row_width=2)
     markup.add(*buttons)
     return markup
 
-def initialize_user_session(session, username):
+def initialize_user(session, username):
+    """
+    Функция для добавления нового пользователя в базу данных
+    :param session: Сессия базы данных.
+    :param username: Имя пользователя.
+    """
     if not check_user_exist(session, username):
         add_user(session, username)
 
 @bot.message_handler(commands=['cards', 'start'])
 def create_cards(message):
+    """
+    Обработчик команд: /cards или /start
+    """
     create_table(engine)
     with Session() as session:
         db_init(session)
-        initialize_user_session(session, message.from_user.username)
+        initialize_user(session, message.from_user.username)
 
     cid = message.chat.id
     if cid not in known_users:
@@ -76,6 +110,9 @@ def create_cards(message):
     update_buttons(message)
 
 def update_buttons(message):
+    """
+    Функция для обновления кнопок с новыми словами.
+    """
     global buttons
     with Session() as session:
         target_word, translate = get_random_word_pair(session, message.from_user.username)
@@ -99,16 +136,26 @@ def update_buttons(message):
 
 @bot.message_handler(func=lambda message: message.text == Command.NEXT)
 def next_cards(message):
+    """
+    Обработчик команды "Дальше ⏭"
+    """
     update_buttons(message)
 
 
 @bot.message_handler(func=lambda message: message.text == Command.DELETE_WORD)
 def handle_delete_word(message):
+    """
+    Обработчик команды "Удалить слово🔙". Запрашивает у пользователя слово которое планируем удалить.
+    """
     bot.send_message(message.chat.id, f'{message.from_user.username}, введите слово которое хотите удалить')
     bot.register_next_step_handler(message, process_delete_word)
 
 
 def process_delete_word(message):
+    """
+    Функция для обработки удаления слова
+    :param message: Сообщение пользователя, содержащее слово для удаления
+    """
     word = message.text.lower()
     with Session() as session:
         if session.query(Words).filter_by(target_word=word).first():
@@ -120,10 +167,16 @@ def process_delete_word(message):
 
 @bot.message_handler(func=lambda message: message.text == Command.ADD_WORD)
 def handle_add_word(message):
+    """
+    Обработчик команды "Добавить слово ➕". Запрашивает у пользователя слово и его перевод.
+    """
     bot.send_message(message.chat.id, f'{message.from_user.username}, введите слово и его перевод')
     bot.register_next_step_handler(message, process_add_word)
 
 def process_add_word(message):
+    """
+    Функция для обработки добавления пары слово-перевод
+    """
     try:
         word, translate = message.text.lower().split()
         with Session() as session:
@@ -140,6 +193,11 @@ def process_add_word(message):
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def message_reply(message):
+    """
+    Обработчик текстовых сообщений от пользователя. Проверяет правильность перевода слова.
+    :param message:
+    :return:
+    """
     text = message.text
     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
         target_word = data['target_word']
